26f45dbf6967145b688e769c5a4ec8955a26c54c
Kyle Graham D'souza <kyled@live.in>
2023-03-17 13:26:20 +0000

    Can check for daily insider trading data and write to CSV

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 import requests
+from datetime import date
+import csv
+
+# Make a list of security codes of BSE group stocks
+security_codes = []
+with open('data/EQ170323.CSV', 'r') as csvfile:
+  reader = csv.reader(csvfile)
+  next(reader)
+  for row in reader:
+    if row[2] == 'A ':
+      security_codes.append(row[0])
+    else:
+      continue
+
+# Check Insider trades only for TODAY
+to_date = date.today().strftime('%d/%m/%Y')
+from_date = to_date
+
 
 def get_insider_data(company):
-  headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"}
-  r = requests.get(f'https://api.bseindia.com/BseIndiaAPI/api/DwnldExcelIT15/w?scripcode={company}&fromdt=&todt=&flag=InsiderTrade15', headers=headers)
-  print(r.text)
+  headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"} 
+  response = requests.get(f'https://api.bseindia.com/BseIndiaAPI/api/DwnldExcelIT15/w?scripcode={company}&fromdt={from_date}&todt={to_date}&flag=InsiderTrade15', headers=headers)
+  if response.status_code == 200:
+    lines = response.content.decode('utf-8').splitlines()
+    if not lines:
+      print(f"{company} No Transaction")
+    else:
+      lines.pop(0)
+      # Write the response content to a CSV file
+      with open('data.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        # Decode the response content as text and write each row to the CSV file
+        for row in lines:
+          print(row)
+          writer.writerow(row.split(','))
+      print('CSV file saved successfully.')
 
-get_insider_data(500510)
+for i in security_codes:
+  get_insider_data(i)
