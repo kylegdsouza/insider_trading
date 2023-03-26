3c1e2599626798318b98e2f8fc56a0d790408118
Kyle Graham D'souza <kyled@live.in>
2023-03-26 16:21:21 +0000

    removed urllib and used requests library instead

--- lambda_function.py
+++ lambda_function.py
@@ -1,39 +1,40 @@
-import urllib.request
+import os
 from datetime import date
-import csv
+import requests
 
 # Make a list of security codes of BSE group stocks
-security_codes = []
-with open('data/EQ170323.CSV', 'r') as csvfile:
-  reader = csv.reader(csvfile)
-  next(reader)
-  for row in reader:
-    if row[2] == 'A ':
-      security_codes.append(row[0])
-    else:
-      continue
+security_codes = ['531489']
+# with open('data/EQ170323.CSV', 'r') as csvfile:
+#   reader = csv.reader(csvfile)
+#   next(reader)
+#   for row in reader:
+#     if row[2] == 'A ':
+#       security_codes.append(row[0])
+#     else:
+#       continue
 
 # Check Insider trades only for TODAY
-to_date = date.today().strftime('%d/%m/%Y')
+to_date = '25/03/2023'
 from_date = to_date
+# to_date = date.today().strftime('%d/%m/%Y')
+# from_date = to_date
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"}
 
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"} 
+def get_insider_data(security_code, to_date, from_date):
+  url = f'https://api.bseindia.com/BseIndiaAPI/api/DwnldExcelIT15/w?scripcode={security_code}&fromdt={from_date}&todt={to_date}&flag=InsiderTrade15'
+  print(url)
+  data = requests.get(url, headers=headers)
+  return data.text
 
-def fetch_data(url, headers):
-    try:
-        req = urllib.request.Request(url, headers=headers)
-        with urllib.request.urlopen(req) as response:
-            data = response.read()
-            return data
-    except urllib.error.HTTPError as e:
-        print(f"HTTP Error: {e.code} {e.reason}")
-    except urllib.error.URLError as e:
-        print(f"URL Error: {e.reason}")
-    except Exception as e:
-        print(f"Error: {str(e)}")
-
-url = str(f'https://api.bseindia.com/BseIndiaAPI/api/DwnldExcelIT15/w?scripcode=500180&fromdt={from_date}&todt={to_date}&flag=InsiderTrade15')
-print(url)
-data = fetch_data(url, headers)
-if data:
-    print(data.decode("utf-8"))
+def send_telegram_message(message):
+  telegram_token = os.environ['telegram_token']
+  telegram_chat_id = os.environ['telegram_chat_id']
+  url = f'https://api.telegram.org/bot{telegram_token}/sendMessage?chat_id={telegram_chat_id}&text={message}'
+  print(url)
+  get_request = requests.get(url)
+  return get_request.status_code
+  
+for security_code in security_codes:
+  data = get_insider_data(security_code, to_date, from_date)
+  if data:
+    send_telegram_message(data)
