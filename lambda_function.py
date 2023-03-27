6f1f4ccf15b23bc3790388b6bc1a08ec623e718f
Kyle Graham D'souza <kyled@live.in>
2023-03-27 16:08:25 +0000

    Implemented pandas dataframes.

--- lambda_function.py
+++ lambda_function.py
@@ -1,24 +1,26 @@
 import os
 from datetime import date
 import requests
+import pandas as pd
+from io import StringIO
+import csv
 
 # Make a list of security codes of BSE group stocks
-security_codes = ['531489']
-# with open('data/EQ170323.CSV', 'r') as csvfile:
-#   reader = csv.reader(csvfile)
-#   next(reader)
-#   for row in reader:
-#     if row[2] == 'A ':
-#       security_codes.append(row[0])
-#     else:
-#       continue
+security_codes = []
+with open('data/EQ170323.CSV', 'r') as csvfile:
+  reader = csv.reader(csvfile)
+  next(reader)
+  for row in reader:
+    if row[2] == 'A ':
+      security_codes.append(row[0])
+    else:
+      continue
 
 # Check Insider trades only for TODAY
-to_date = '25/03/2023'
+to_date = date.today().strftime('%d/%m/%Y')
 from_date = to_date
-# to_date = date.today().strftime('%d/%m/%Y')
-# from_date = to_date
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"}
+df = pd.DataFrame()
 
 def get_insider_data(security_code, to_date, from_date):
   url = f'https://api.bseindia.com/BseIndiaAPI/api/DwnldExcelIT15/w?scripcode={security_code}&fromdt={from_date}&todt={to_date}&flag=InsiderTrade15'
@@ -34,7 +36,15 @@ def send_telegram_message(message):
   get_request = requests.get(url)
   return get_request.status_code
   
+def create_dataframe_from_csv(csv_data):
+  csv_data_io = StringIO(csv_data)
+  df = pd.read_csv(csv_data_io)
+  return df
+
 for security_code in security_codes:
   data = get_insider_data(security_code, to_date, from_date)
   if data:
-    send_telegram_message(data)
+    df = df.append(create_dataframe_from_csv(data))
+
+print(df.info())
+    
