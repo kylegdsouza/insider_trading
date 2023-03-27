7769f4e63adeb5bc29df757da439eeb8f657e526
Kyle Graham D'souza <kyled@live.in>
2023-03-27 16:30:32 +0000

    Filtered data and now able to send market purchase data summed by company

--- lambda_function.py
+++ lambda_function.py
@@ -6,15 +6,15 @@ from io import StringIO
 import csv
 
 # Make a list of security codes of BSE group stocks
-security_codes = []
-with open('data/EQ170323.CSV', 'r') as csvfile:
-  reader = csv.reader(csvfile)
-  next(reader)
-  for row in reader:
-    if row[2] == 'A ':
-      security_codes.append(row[0])
-    else:
-      continue
+security_codes = ['500003','500085','500265','500331','500510','505283','506943','508869','509480','512070','512179','512237','512455','513262','523405','531162','532187','532504','532523','532539','532683','532892','532927','533326','541403','542651']
+# with open('data/EQ170323.CSV', 'r') as csvfile:
+#   reader = csv.reader(csvfile)
+#   next(reader)
+#   for row in reader:
+#     if row[2] == 'A ':
+#       security_codes.append(row[0])
+#     else:
+#       continue
 
 # Check Insider trades only for TODAY
 to_date = date.today().strftime('%d/%m/%Y')
@@ -47,4 +47,8 @@ for security_code in security_codes:
     df = df.append(create_dataframe_from_csv(data))
 
 print(df.info())
-    
+market_purchase = df.loc[df['Mode of Acquisition'] == 'Market Purchase']
+
+summed = market_purchase.groupby('Security Name')['Value  of Securities Acquired/Disposed/Pledge etc'].sum()
+
+send_telegram_message(str(summed))
