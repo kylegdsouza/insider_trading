69faf8812bdb31cc6c27a2cca68458ad047bab3c
Kyle Graham D'souza <kyled@live.in>
2023-03-30 17:36:26 +0000

    sorted the data and changed the format of the numbers. removed the security list approach.

--- lambda_function.py
+++ lambda_function.py
@@ -3,27 +3,17 @@ from datetime import date
 import requests
 import pandas as pd
 from io import StringIO
-import csv
-
-# Make a list of security codes of BSE group stocks
-security_codes = ['500003','500085','500265','500331','500510','505283','506943','508869','509480','512070','512179','512237','512455','513262','523405','531162','532187','532504','532523','532539','532683','532892','532927','533326','541403','542651']
-# with open('data/EQ170323.CSV', 'r') as csvfile:
-#   reader = csv.reader(csvfile)
-#   next(reader)
-#   for row in reader:
-#     if row[2] == 'A ':
-#       security_codes.append(row[0])
-#     else:
-#       continue
 
 # Check Insider trades only for TODAY
 to_date = date.today().strftime('%d/%m/%Y')
 from_date = to_date
+
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"}
+
 df = pd.DataFrame()
 
-def get_insider_data(security_code, to_date, from_date):
-  url = f'https://api.bseindia.com/BseIndiaAPI/api/DwnldExcelIT15/w?scripcode={security_code}&fromdt={from_date}&todt={to_date}&flag=InsiderTrade15'
+def get_insider_data(to_date, from_date):
+  url = f'https://api.bseindia.com/BseIndiaAPI/api/DwnldExcelIT15/w?scripcode=&fromdt={from_date}&todt={to_date}&flag=InsiderTrade15'
   print(url)
   data = requests.get(url, headers=headers)
   return data.text
@@ -41,14 +31,18 @@ def create_dataframe_from_csv(csv_data):
   df = pd.read_csv(csv_data_io)
   return df
 
-for security_code in security_codes:
-  data = get_insider_data(security_code, to_date, from_date)
-  if data:
-    df = df.append(create_dataframe_from_csv(data))
 
+data = get_insider_data(to_date, from_date)
+if data:
+  df = df.append(create_dataframe_from_csv(data))
+  
 print(df.info())
 market_purchase = df.loc[df['Mode of Acquisition'] == 'Market Purchase']
+def unit_lakhs(x):
+  return round(x/100000,2)
 
 summed = market_purchase.groupby('Security Name')['Value  of Securities Acquired/Disposed/Pledge etc'].sum()
+sorted_summed = summed.sort_values(ascending=False)
+lakhs_summed = sorted_summed.apply(unit_lakhs)
 
-send_telegram_message(str(summed))
+send_telegram_message(lakhs_summed)
